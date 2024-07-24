0f0f1b1615de66cc278c27ebe5ccf91be28c9a7b
Alex KooWeso <koowa360@gmail.com>
2024-07-24 14:06:30 +0300

    all good now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Button } from 'antd'
 import './App.css'
 
 function App() {
-  return <div>Hello Worlds!</div>
+  return <Button type="primary">Hello Worlds!</Button>
 }
 
 export default App
